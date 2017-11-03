a85a97713f8bd5550f76ac180d33611696ce6c28
Mopkau <info@gromoplan.com.ua>
2017-11-03 11:17:10 +0200

    upd

--- actions/NotifierAction.php
+++ actions/NotifierAction.php
@@ -8,7 +8,6 @@
 
 namespace bulk\actions;
 
-
 use common\components\MainModel;
 use notifier\helpers\HistoryHelper;
 use notifier\helpers\SendHelper;
@@ -16,15 +15,35 @@ use notifier\models\db\NotifierTemplates;
 use yii\base\Action;
 use yii\db\ActiveRecord;
 use yii\web\Controller;
-
+/**
+ * Class NotifierAction
+ * @package bulk\actions
+ */
 class NotifierAction extends Action
 {
+    /**
+     * @var string
+     */
     public $attribute = 'bulk';
 
+    /**
+     * @var $_ids
+     */
     private $_ids;
 
+    /**
+     * @var $_templateID
+     */
     private $_templateID;
 
+    /**
+     * @var array
+     */
+    public $callbacks = [];
+
+    /**
+     *
+     */
     public function init()
     {
         parent::init();
@@ -32,19 +51,49 @@ class NotifierAction extends Action
         $this->setIds(\Yii::$app->request->post($this->attribute));
     }
 
+    /**
+     * @return mixed
+     */
     public function run(){
-        $sender = new SendHelper($this->prepareModels()->all() , new HistoryHelper(), NotifierTemplates::findOne(['id'=>$this->_templateID]));
-        $sender->notify();
+        foreach ($this->prepareModels()->all() as $model){
+
+            $service = new SendHelper(new HistoryHelper(), $this->loadTemplate($model,$model->language));
+            $service->send($model);
+        }
+
 
         return $this->prepareModels()->count();
+    }
 
+    /**
+     * @param $model MainModel
+     * @param $lang string
+     * @return static
+     */
+    public function loadTemplate($model, $lang){
+
+        $template = NotifierTemplates::find()->localized($lang)->where(['id'=>$this->_templateID]);
+
+        if($template->label === 'Форма посетителя [EN]' || $template->label === 'Форма посетителя'){
+
+            str_replace( '{{$ticket_url}}', $model->pdfPath, $template->message );
+
+        }
+
+        return $template;
 
     }
 
+    /**
+     * @param $value
+     */
     private function setIds($value){
         $this->_ids = $value;
     }
 
+    /**
+     * @return mixed
+     */
     private function prepareModels(){
         $class = $this->controller->getModelClass();
         $searcher = new $class;
